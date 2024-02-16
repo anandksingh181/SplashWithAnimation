540d7017c048e93df705f3bcd7277cc3c1e4ecd1
anandksingh181 <142203756+anandksingh181@users.noreply.github.com>
2024-02-16 22:38:31 +0530

    first commit

---
+++ app/src/main/java/com/example/splashwithanimation/SpWithAnimationActivity.kt
@@ -0,0 +1,11 @@
+package com.example.splashwithanimation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SpWithAnimationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sp_with_animation)
+    }
+}
